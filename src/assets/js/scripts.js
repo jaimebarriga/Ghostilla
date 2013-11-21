2f020cf7570a847f0bc5dc55ac4f859efd571f39
Jaime Barriga <jab.colo33@gmail.com>
2013-11-21 04:15:25 -0500

    fix post header bug

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -177,8 +177,8 @@
     var mainImage = $('img[alt="main-image"]');
   
     if ( mainImage.length > 0){
-        mainImageSource = mainImage.attr('src');
-        $('header').css('background-image','url('+mainImageSource+')');
+        var mainImageSource = mainImage.attr('src');
+        $('#site-head').css('background-image','url('+mainImageSource+')');
         mainImage.remove();
     }
 
